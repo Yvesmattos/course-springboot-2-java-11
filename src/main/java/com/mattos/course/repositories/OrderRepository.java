9074711055cd74f73f1d84313772bc1c6db60846
Yves Mattos <yvesmattos@gmail.com>
2020-06-29 20:55:13 -0300

    Order, Instant, ISO 8601 - PART 2

---
+++ src/main/java/com/mattos/course/repositories/OrderRepository.java
@@ -0,0 +1,13 @@
+package com.mattos.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mattos.course.entities.Order;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long>{
+	
+	
+
+}
