f9400875acd874cd773337f92ffd11661c69b774
Yves Mattos <yvesmattos@gmail.com>
2020-06-30 19:31:32 -0300

    Product

---
+++ src/main/java/com/mattos/course/repositories/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.mattos.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mattos.course.entities.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>{
+	
+	
+
+}
